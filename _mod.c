74e89ce3bc199a86ff81ed216b19a22feda1cd59
bneayoub <bne.ayoub@gmail.com>
2023-06-23 00:53:20 +0100

    modulo implementation

---
+++ _mod.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * _mod - check code
+ * @stack: ptr to stack
+ * @line_num: the line
+ */
+
+void _mod(stack_t **stack, unsigned int line_number)
+{
+
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n %= (*stack)->n;
+
+	_pop(stack, line_number);
+}
