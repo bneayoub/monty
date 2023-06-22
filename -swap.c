50530ceef64076d703162d0cf67d88640422d3dc
bneayoub <bne.ayoub@gmail.com>
2023-06-23 00:49:24 +0100

    swap function

---
+++ -swap.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * _swap - check code
+ * @stack: ptr to stack
+ * @line_num: the line
+ */
+
+void _swap(stack_t **stack, unsigned int line_number)
+{
+	int swaping;
+
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	swaping = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = swaping;
+}
+
