1879b4376b25e5ccb86a426ea4553bbf70ce6b78
bneayoub <bne.ayoub@gmail.com>
2023-06-23 00:50:46 +0100

    nothing to push

---
+++ _nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * _nop - check code
+ * @stack: ptr to stack
+ * @line_num: the line
+ */
+void _nop(stack_t **stack, unsigned int line_num)
+{
+	(void)stack;
+	(void)line_num;
+}
