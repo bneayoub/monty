8c455a17fc9cea911cbe8be8ce7de6388526994f
bneayoub <bne.ayoub@gmail.com>
2023-06-23 00:52:23 +0100

    substraction implementation

---
+++ _sub.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+  * _sub - check code
+ * @stack: ptr to stack
+ * @line_num: the line
+ */
+
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *element = *stack;
+	int sub_op;
+
+	if (element == NULL || element->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sub_op = (element->next->n) - (element->n);
+	_pop(stack, line_number);
+	(*stack)->n = sub_op;
+}
