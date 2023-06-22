022d250d2abe4d8eacf57d9ee961134a9218e528
bneayoub <bne.ayoub@gmail.com>
2023-06-23 00:52:53 +0100

    multiplication

---
+++ _mul.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * _mul - check code
+ * @stack: ptr to stack
+ * @line_num: the line
+ */
+
+void _mul(stack_t **stack, unsigned int line_number)
+{
+
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n *= (*stack)->n;
+
+	_pop(stack, line_number);
+}
