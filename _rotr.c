ea5ebf1f929e951e0908bd6ca2223f5d0de26949
bneayoub <bne.ayoub@gmail.com>
2023-06-23 00:55:16 +0100

    rotc

---
+++ _rotr.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+
+/**
+ * _rotr - check code
+ * @stack: ptr to stack
+ * @line_num: the line
+ */
+
+void _rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+	(void)line_number;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+
+	current = *stack;
+
+	while (current->next != NULL)
+		current = current->next;
+
+	current->next = *stack;
+	current->prev->next = NULL;
+	current->prev = NULL;
+	(*stack)->prev = current;
+	*stack = current;
+}
