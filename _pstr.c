207f96fd60c89ed5bf315dc331ee39e8f5fc5946
bneayoub <bne.ayoub@gmail.com>
2023-06-23 00:54:21 +0100

    priint string

---
+++ _pstr.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * _pstr - check code
+ * @stack: ptr to stack
+ * @line_num: the line
+ */
+void _pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	(void)line_number;
+
+	while (current != NULL)
+	{
+		if (current->n > 0 && current->n <= 127)
+			printf("%c", current->n);
+		else
+			break;
+		current = current->next;
+	}
+	printf("\n");
+}
