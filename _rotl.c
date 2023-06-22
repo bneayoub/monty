9818429cb30184ca8f920e84f698efc893bedc8d
bneayoub <bne.ayoub@gmail.com>
2023-06-23 00:55:08 +0100

    rotl impl

---
+++ _rotl.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+
+/**
+ * _rotl - check code
+ * @stack: ptr to stack
+ * @line_num: the line
+ */
+void _rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current, *tail;
+	(void)line_number;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+	current = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+
+	tail = *stack;
+	while (tail->next != NULL)
+		tail = tail->next;
+
+	tail->next = current;
+	current->prev = tail;
+	current->next = NULL;
+}
