d187d439d3945f439bbe6499dfff2a1ea6ea1e14
bneayoub <bne.ayoub@gmail.com>
2023-06-23 00:52:12 +0100

    division

---
+++ _div.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * _div - check code
+ * @stack: ptr to stack
+ * @line_num: the line
+ */
+
+void _div(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n /= (*stack)->n;
+	_pop(stack, line_number);
+}
